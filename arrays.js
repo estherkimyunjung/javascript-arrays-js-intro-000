b4fe7a14c88c28d3f0e4915fd526cdab39d24bb6
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 02:49:26 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,13 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element);
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  [element, ...array];
 }
 
 function addElementToEndOfArray(array, element) {
-  array.push(element);
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  [...array, element];
 }
