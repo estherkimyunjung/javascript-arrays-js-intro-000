325f3f5c3fb5a033d4b3c43bc651cc46aa6f20d0
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 03:19:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,9 +24,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
-  
-  
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array
 }
-
-function 
