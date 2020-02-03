cca6445a430e03873f3aff5f37f08453bc6736cb
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 01:56:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  chocolateBars.push("add")
+  chocolateBars.push("Toblerone");
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
