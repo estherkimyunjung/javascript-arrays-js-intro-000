70bb1fa1307af8435554af14c66175e2dd545715
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-02-03 02:43:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,5 +13,5 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  [...chocolateBars,element]
+  var array = [...chocolateBars,element]
 }
